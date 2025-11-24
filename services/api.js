b64a1d677a1f043143412ee11593ac2eb09e2181
utkarshtyagi1915 <utkarsh.tyagi@onmeridian.com>
2025-11-24 11:21:42 +0530

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://ats-new-backend-ave7edeebycda8g0.centralindia-01.azurewebsites.net/api',
 });
 
 export default api;
