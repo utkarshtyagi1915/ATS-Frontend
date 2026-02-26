4362f6d5bbc5f2be8a8b6e699982d090dce80c84
vinaysaraswat33 <vinaysaraswat33@gmail.com>
2026-02-26 16:17:40 +0530

    Updated changes

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: 'https://ats-backends.azurewebsites.net/api'
 });
 
 export default api;
